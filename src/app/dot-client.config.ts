9fefe56f817706122cc49ea1e27a419244137f9c
Nicolas Molina <hi@nicobytes.com>
2025-06-06 11:03:31 -0400

    Add a comment in dot-client.config.ts to clarify the use of a hack inspired by AngularFire for better understanding of the code.

--- src/app/dot-client.config.ts
+++ src/app/dot-client.config.ts
@@ -4,6 +4,8 @@ import { DotCMSClientConfig } from '@dotcms/types';
 
 type clientType = ReturnType<typeof createDotCMSClient>;
 
+// This is a hack inspired by https://github.com/angular/angularfire/blob/c1c6af9779154caff6bc0d9b837f6c3e2d913456/src/firestore/firestore.ts#L8
+
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-unsafe-declaration-merging
 export interface DotCMSClient extends clientType {}
 
